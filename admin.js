c2c472ea8a62a4fd091312f9bd6ad86870d77a15
princekamboj94670-maker <princekamboj94670@gmail.com>
2026-08-08 23:38:37 +0530

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,119 @@
+import { db } from "./firebase.js";
+
+import {
+  collection,
+  getDocs,
+  deleteDoc,
+  doc
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+const ordersDiv = document.getElementById("orders");
+
+async function loadOrders() {
+
+    ordersDiv.innerHTML = "<h2>Loading Orders...</h2>";
+
+    try {
+
+        const querySnapshot = await getDocs(collection(db, "orders"));
+
+        if (querySnapshot.empty) {
+            ordersDiv.innerHTML = "<h2>No Orders Found</h2>";
+            return;
+        }
+
+        ordersDiv.innerHTML = "";
+
+        querySnapshot.forEach((orderDoc) => {
+
+            const order = orderDoc.data();
+
+            let cartItems = "";
+
+            if (order.cart && order.cart.length > 0) {
+
+                order.cart.forEach(item => {
+
+                    cartItems += `
+                    <li>
+                    ${item.name} × ${item.qty}
+                    </li>
+                    `;
+
+                });
+
+            } else {
+
+                cartItems = "<li>No Products</li>";
+
+            }
+
+            ordersDiv.innerHTML += `
+
+            <div style="
+            border:1px solid #ccc;
+            padding:15px;
+            margin:15px;
+            border-radius:10px;
+            background:#f7f7f7;
+            ">
+
+            <h2>Order ID</h2>
+
+            <p>${orderDoc.id}</p>
+
+            <p><b>Name:</b> ${order.name}</p>
+
+            <p><b>Phone:</b> ${order.phone}</p>
+
+            <p><b>Address:</b> ${order.address}</p>
+
+            <p><b>City:</b> ${order.city}</p>
+
+            <p><b>Pincode:</b> ${order.pincode}</p>
+
+            <p><b>Payment:</b> ${order.payment}</p>
+
+            <p><b>Date:</b> ${order.date}</p>
+
+            <h3>Products</h3>
+
+            <ul>
+            ${cartItems}
+            </ul>
+           <button onclick="editProduct('${doc.id}')">✏️ Edit</button>
+            <button onclick="deleteOrder('${orderDoc.id}')">
+            Delete Order
+            </button>
+
+            </div>
+
+            `;
+
+        });
+
+    } catch (error) {
+
+        ordersDiv.innerHTML = "<h2>Error Loading Orders</h2>";
+
+        alert(error.message);
+
+    }
+
+}
+
+window.deleteOrder = async function(id){
+
+    if(confirm("Delete this order?")){
+
+        await deleteDoc(doc(db,"orders",id));
+
+        alert("Order Deleted");
+
+        loadOrders();
+
+    }
+
+}
+
+loadOrders();
