d92de7cfcb8c9de32d06aba82a967f73a357b2cb
princekamboj94670-maker <princekamboj94670@gmail.com>
2026-08-17 14:03:45 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import console.log("Login JS Loaded"); { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
 
 import {
   getAuth,
