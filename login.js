9172b0bc0b8bb1e9a321340f76294f325b0ca416
princekamboj94670-maker <princekamboj94670@gmail.com>
2026-08-17 14:01:07 +0530

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,6 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebas
 
 import {
   getAuth,
-  createUserWithEmailAndPassword,
   signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
 
@@ -18,34 +17,38 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-window.signup = function () {
+const loginForm =
+document.getElementById("loginForm");
 
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+loginForm.addEventListener(
+"submit",
+async function(e){
 
-  createUserWithEmailAndPassword(auth, email, password)
-    .then(() => {
-      alert("Signup Successful");
-      window.location.href = "index.html";
-    })
-    .catch(error => {
-      alert(error.message);
-    });
+e.preventDefault();
 
-};
+const email =
+document.getElementById("email").value.trim();
 
-window.login = function () {
+const password =
+document.getElementById("password").value.trim();
 
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+try{
 
-  signInWithEmailAndPassword(auth, email, password)
-    .then(() => {
-      alert("Login Successful");
-      window.location.href = "index.html";
-    })
-    .catch(error => {
-      alert(error.message);
-    });
+await signInWithEmailAndPassword(
+auth,
+email,
+password
+);
 
-};
+alert("✅ Login Successful");
+
+window.location.href =
+"index.html";
+
+}catch(error){
+
+alert(error.message);
+
+}
+
+});
